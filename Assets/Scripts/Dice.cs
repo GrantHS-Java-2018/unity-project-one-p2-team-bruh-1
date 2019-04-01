acf2ad468d148e8178712cd975a86d34287684b0
Dom <d0md0ntcar@gmail.com>
2019-04-01 11:38:19 -0700

    added in movemnt for players

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -6,20 +6,24 @@ public class Dice : MonoBehaviour {
 
     private Sprite[] diceSides;
     private SpriteRenderer rend;
-
+    private int whosTurn = 1;
+    private bool coroutineAllowed = true;
     private void Start () {
 
         rend = GetComponent<SpriteRenderer>();
         diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+        rend.sprite = diceSides[5];
     }
 	
     private void OnMouseDown()
     {
+        if(!GameControl.gameOver && coroutineAllowed)
         StartCoroutine("RollTheDice");
     }
 
     private IEnumerator RollTheDice()
     {
+        coroutineAllowed = false;
         int randomDiceSide = 0;
         int finalSide = 0;
         for (int i = 0; i <= 20; i++)
@@ -30,5 +34,17 @@ public class Dice : MonoBehaviour {
         }
         finalSide = randomDiceSide + 1;
         Debug.Log(finalSide);
+
+        GameControl.diceSideThrown = randomDiceSide + 1;
+        if (whosTurn == 1)
+        {
+            GameControl.MovePlayer(1);
+        } else if (whosTurn == -1)
+        {
+            GameControl.MovePlayer(2);
+        }
+
+        whosTurn *= -1;
+        coroutineAllowed = true;
     }
 }
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+
+	private static GameObject whoWinsText, player1MoveText, Player2MoveText;
+
+	private static GameObject player1, player2;
+
+	public static int diceSideThrown = 0;
+
+	public static int player1StartWaypoint = 0;
+	public static int player2StartWaypoint = 0;
+
+	public static bool gameOver = false;
+	
+	// Use this for initialization
+	void Start ()
+	{
+
+		whoWinsText = GameObject.Find("WhoWinsText");
+		player1MoveText = GameObject.Find("Player1MoveText");
+		Player2MoveText = GameObject.Find("Player2MoveText");
+		
+		player1 = GameObject.Find("player1");
+		player2 = GameObject.Find("player2");
+
+		player1.GetComponent<FollowingPath>().moveAllowed = false;
+		player2.GetComponent<FollowingPath>().moveAllowed = false;
+		
+		whoWinsText.gameObject.SetActive(false);
+		player1MoveText.gameObject.SetActive(true);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
