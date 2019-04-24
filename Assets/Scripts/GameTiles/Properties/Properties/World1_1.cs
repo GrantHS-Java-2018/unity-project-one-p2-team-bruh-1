d73f57f0db1a1d24d1fe3244d53bde416c354f02
Eddie Peterson <etppdx@gmail.com>
2019-04-24 11:57:36 -0700

    Why did the script come off all the tiles again

--- Assets/Scripts/GameTiles/Properties/Properties/World1_1.cs
+++ Assets/Scripts/GameTiles/Properties/Properties/World1_1.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.Networking.NetworkSystem;
 
 public class World1_1 : PropertyMasterScript
 {
@@ -14,40 +16,55 @@ public class World1_1 : PropertyMasterScript
 
 	}
 
-	
-	
-	
 	public override void doThing(GameObject playerMovingHere)
 	{
+		if (playerMovingHere.Equals(player1))
+		{
+			if (purchased == false) //If it's purchased
+            		{
+            			if (GetComponent<Button>().answer == true) //If the player wants to buy it
+            				if (playerMovingHere.GetComponent<PlayerMasterScript>().getBalance() >= price) //If the player has enough money to buy it
+            				{
+            					purchased = true;
+            					GetComponent<P1Script>().setBalance(-(this.price));
+            					GetComponent<P2Script>().setBalance(this.price);
+            				}
+            			
+            		}
+            
+            		else if (purchased == true)
+            		{
+            			GetComponent<P1Script>().setBalance(-(rentArray[hotels]));
+            			GetComponent<P2Script>().setBalance(rentArray[hotels]);
+            		}
+		}
+		else if (playerMovingHere.Equals(player2))
+		{
+			if (purchased == false) //If it's purchased
+			{
+				if (GetComponent<Button>().answer == true) //If the player wants to buy it
+					if (playerMovingHere.GetComponent<PlayerMasterScript>().getBalance() >= price) //If the player has enough money to buy it
+					{
+						purchased = true;
+						GetComponent<P2Script>().setBalance(-(this.price));
+						GetComponent<P1Script>().setBalance(this.price);
+					}
+            			
+			}
+            
+			else if (purchased == true)
+			{
+				GetComponent<P2Script>().setBalance(-(rentArray[hotels]));
+				GetComponent<P1Script>().setBalance(rentArray[hotels]);
+			}
+		}
+		else
+		{
+			Debug.Log("It did not recognize which player landed on this space");
+		}
 
 		
-		if (purchased == false)
-		{
-			//Prompt: do you want to buy this property?
-			//If (wantToBuy == true){
-				/* if the player has enough money to buy it{
-				 * 			purchased == true
-				 * 			money.player-= price
-				 *	}
-			}*/
-				if (playerMovingHere.GetComponent<PlayerMasterScript>().getBalance() >= price)
-				{
-					
-				}
-			
-			
-			
-		}
-
-		else if (purchased == true)
-		{
-			
-			
-			
-			GetComponent<P1Script>().setBalance(-(rentArray[hotels]));
-			GetComponent<P2Script>().setBalance(rentArray[hotels]);
-			
-		}
+		
 	}
 
 	public int rent(int hotels)
